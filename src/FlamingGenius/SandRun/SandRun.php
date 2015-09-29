9a2d5e27e74dfd5e65aa7237dd941c9e1703efab
FlamingGenius <livewiretech.reply.mail@gmail.com>
2015-09-29 11:31:32 -0400

    Create SandRun.php

---
+++ src/FlamingGenius/SandRun/SandRun.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace FlamingGenius\SandRun;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\event\Listener;
+
+class SandRun extends PluginBase implements Listener{
+ public $timer = [];
+ public function onEnable(){
+  $this->saveDefaultConfig();
+ }
+ 
+ public function createTask(){
+  $timer = new Task($this);
+  $h = $this->getServer()->getScheduler()->scheduleRepeatingTask($timer, 20);
+  $timer->setHandler($handler);
+  $this->timer[$timer->getTaskId] = $timer->getTaskId();
+ }
+
+ public function onPlayerJoin(PlayerJoinEvent $event){
+  $player = $event->getPlayer();
+  $playerName = $player->getName();
+ }
+
+
+}
+
+
+
+
+?>
