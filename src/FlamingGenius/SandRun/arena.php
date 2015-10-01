ac81b3badc43d4cbe966b1d40f7befde1c211eb4
FlamingGenius <livewiretech.reply.mail@gmail.com>
2015-10-01 15:17:49 -0400

    Update arena.php

--- src/FlamingGenius/SandRun/arena.php
+++ src/FlamingGenius/SandRun/arena.php
@@ -2,9 +2,16 @@
 
 namespace FlamingGenius\SandRun;
 
+use pocketmine\utils\TextFormat;
+use pocketmine\event\PlayerJoinEvent;
 
-
-
-
-
+class arena{
+  public function onArenaJoin(PlayerJoinEvent $event){
+    $players = $event->getServer()->getOnlinePlayers();
+    foreach($players as $player){
+     $player->getName();
+     $this->getServer()->broadcastMessage($player . "Has Connected");
+    }
+  }
+}
 ?>
