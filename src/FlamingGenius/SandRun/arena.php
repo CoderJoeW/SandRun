3714c7cd698cad4a9d325825f9cd7162938ebfd6
FlamingGenius <livewiretech.reply.mail@gmail.com>
2015-10-01 18:11:55 -0400

    Update arena.php

--- src/FlamingGenius/SandRun/arena.php
+++ src/FlamingGenius/SandRun/arena.php
@@ -6,12 +6,14 @@ use pocketmine\utils\TextFormat;
 use pocketmine\event\PlayerJoinEvent;
 
 class arena{
-  public function onArenaJoin(PlayerJoinEvent $event){
-    $players = $this->getServer()->getOnlinePlayers();
-    foreach($players as $player){
-      $name = $event->getPlayer()->getName();
-      $this->getServer()->broadcastMessage($name . "Has Connected");
-    }
+ $arena = $this->getConfig("arena")->get("arena-world");
+ 
+ public function onArenaJoin(PlayerJoinEvent $event){
+  $players = $this->getServer()->getOnlinePlayers();
+  foreach($players as $player){
+   $name = $event->getPlayer()->getName();
+   $this->getServer()->broadcastMessage($name . "Has Connected");
   }
+ }
 }
 ?>
