b573939f992df6ff11e1fa1cab4e4aa4d980b9a5
Victor T. <pro_victor@ymail.com>
2015-09-30 21:26:33 -0700

    Update SandRun.php

--- src/FlamingGenius/SandRun/SandRun.php
+++ src/FlamingGenius/SandRun/SandRun.php
@@ -1,40 +1,26 @@
 <?php
-
 namespace FlamingGenius\SandRun;
-
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\event\Event;
-
 class SandRun extends PluginBase implements Listener{
  public $timer = [];
  public function onEnable(){
   $this->saveDefaultConfig();
   $this->getServer()->getPluginManager()->registerEvents($this, $this);
  }
- 
  public function createTask(){
   $timer = new Task($this);
   $h = $this->getServer()->getScheduler()->scheduleRepeatingTask($timer, 20);
-  $timer->setHandler($handler);
   $this->timer[$timer->getTaskId] = $timer->getTaskId();
  }
-
- 
  public function onLobbyJoin(){
   $lobby = $this->getConfig()->get("lobby-world");
-  $players = $this->getServer()->getLevelByName($lobby)->getOnlinePlayers();
+  $players = count($this->getServer()->getLevelByName($lobby)->getPlayers());
   $min = $this->getConfig()->get("min-players");
-  if(count($players) >= $min){
-   
+  if($players >= $min){
+   //idk what you want to do here, so i'll just leave this alone
   }
  }
-
-
 }
-
-
-
-
-?>
